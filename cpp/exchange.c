8da40f5a383651664c5b4a7238bf72d34e257da6
lofei117 <lofei117@gmail.com>
2012-09-26 13:12:07 +0800

    add an c file which can exchange theodd bit with the even bit

---
+++ cpp/exchange.c
@@ -0,0 +1,17 @@
+/**
+ *  exchange - exchange even bit and odd bit of an Integer number.
+ *  AUTHOR: Lofei Lin (lofei117@qq.com)
+ *  TIME: 09/26/2012
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+#define ODD_MASK 0x555555
+#define EVEN_MASK 0xAAAAAA
+#define EXCHANGE(x) (((x)&ODD_MASK)<<1)|(((x)&EVEN_MASK)>>1)
+
+int main(int argc, char** args)
+{
+	if(argc>1)
+		printf("%d, %d\n", atoi(args[1]), EXCHANGE(atoi(args[1])));
+	return 0;
+}
